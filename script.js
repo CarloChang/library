0c2e42c4aff0c41341463b93ab7e7edbe7e82b68
Carlo Chang <carlochang123@hotmail.com>
2024-08-11 21:41:12 +0200

    Added a functional modal

--- script.js
+++ script.js
@@ -1,44 +1,86 @@
 const myLibrary = [];
+let id = 0;
 
-function Book(title, author, pages, selection, id){
+function Book(title, author, pages, read, id){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.selection = selection;
+    this.read = read;
     this.id = id;
 }
 
-function addBookToLibrary(){
-
-    if (title == "" && author == "" && pages == "" && selection == ""){
-        alert('No pueden quedar vacíos');
-    } else {
+function addBookToLibrary(newTitle, newAuthor, newPages, newRead){
         id++;
-        myLibrary.push(new Book(title, author, pages, selection, id));
+        myLibrary.push(new Book(newTitle, newAuthor, newPages, newRead, id));  
 
-    }    
+        myLibrary.forEach(book => {
+            const bookElement = document.createElement('div');
+            bookElement.className = 'book';
+            const titleElement = document.createElement('div');
+            titleElement.className = 'title';
+            const authorElement = document.createElement('div');
+            authorElement.className = 'author';
+            const pagesElement = document.createElement('div');
+            pagesElement.className = 'pages';
+            const readElement = document.createElement('div');
+            readElement.className = 'read';
+    
+            titleElement.textContent = newTitle;
+            authorElement.textContent = newAuthor;
+            pagesElement.textContent = newPages;
+            readElement.textContent = newRead;
+    
+            bookElement.appendChild(titleElement);
+            bookElement.appendChild(authorElement);
+            bookElement.appendChild(pagesElement);
+            bookElement.appendChild(readElement);
+
+            books.appendChild(bookElement);
+            console.log(book.title);
+        });
 }
+
+
+
+const books = document.getElementById('books');
+
 const dialog = document.getElementById('myDialog');
+
 const cancel = document.getElementById('cancel').addEventListener('click', (e)=>{
     e.preventDefault();
     alert('No book added');
     dialog.close();
 });
-cancel
+
 const add = document.getElementById('add');
-add.addEventListener('click', ()=>{
-    
+
+add.addEventListener('click', (e)=>{
+    e.preventDefault();
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked;
+
+    if (title == "" || author == "" || pages == ""){
+        alert('You should not leave empty spaces.');
+    }
+    if (isNaN(pages)){
+        alert('The number of pages should be a numeric value');
+    }
+
+    else {
+        addBookToLibrary(title, author, pages, read);
+        alert('Book added successfully');
+        dialog.close();
+    }
 })
 
 const newBookButton = document.getElementById('newBook');
 newBookButton.addEventListener('click', ()=>{
     dialog.showModal();
 });
-let id = 0;
-let title = document.getElementById('title');
-let author = document.getElementById('author');
-let pages = document.getElementById('pages');
-let read = document.getElementById('read');
+
+
 
 
 
@@ -46,6 +88,5 @@ Book.prototype.callBook = function (){
     console.log(this.title, this.author, this.pages, this.selection, this.id);
 }
 
-myLibrary.forEach(book => {
-    
-});
+
+
