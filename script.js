bc628e31369f4174e55e93ffabad23f71f756384
Carlo Chang <carlochang123@hotmail.com>
2024-08-14 17:41:23 +0200

    Added delete button

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const myLibrary = [];
-let id = 0;
-
 function Book(title, author, pages, read, id){
     this.title = title;
     this.author = author;
@@ -9,36 +6,46 @@ function Book(title, author, pages, read, id){
     this.id = id;
 }
 
-function addBookToLibrary(newTitle, newAuthor, newPages, newRead){
-        id++;
-        myLibrary.push(new Book(newTitle, newAuthor, newPages, newRead, id));  
+function addBookToLibrary(newTitle, newAuthor, newPages, newRead) {
+    const bookElement = document.createElement('div');
+    bookElement.className = 'book';
+    const titleElement = document.createElement('div');
+    titleElement.className = 'title';
+    const authorElement = document.createElement('div');
+    authorElement.className = 'author';
+    const pagesElement = document.createElement('div');
+    pagesElement.className = 'pages';
+    const readElement = document.createElement('div');
+    readElement.className = 'read';
 
-        myLibrary.forEach(book => {
-            const bookElement = document.createElement('div');
-            bookElement.className = 'book';
-            const titleElement = document.createElement('div');
-            titleElement.className = 'title';
-            const authorElement = document.createElement('div');
-            authorElement.className = 'author';
-            const pagesElement = document.createElement('div');
-            pagesElement.className = 'pages';
-            const readElement = document.createElement('div');
-            readElement.className = 'read';
+    const deleteElement = document.createElement('button');
+    deleteElement.id = 'delete';
+    deleteElement.textContent = 'Delete Book'
     
-            titleElement.textContent = newTitle;
-            authorElement.textContent = newAuthor;
-            pagesElement.textContent = newPages;
-            readElement.textContent = newRead;
-    
-            bookElement.appendChild(titleElement);
-            bookElement.appendChild(authorElement);
-            bookElement.appendChild(pagesElement);
-            bookElement.appendChild(readElement);
+    id++;
+    const newBook = new Book(newTitle, newAuthor, newPages, newRead, id);
+    myLibrary.push(newBook);
 
-            books.appendChild(bookElement);
-            console.log(book.title);
-        });
+    titleElement.textContent = newBook.title;
+    authorElement.textContent = newBook.author;
+    pagesElement.textContent = newBook.pages;
+    if (newBook.read == true){
+        readElement.textContent = "Already read this book!";
+    } else {
+        readElement.textContent = "I haven't read this book yet :(";
+    }
+
+    bookElement.appendChild(titleElement);
+    bookElement.appendChild(authorElement);
+    bookElement.appendChild(pagesElement);
+    bookElement.appendChild(readElement);
+    bookElement.appendChild(deleteElement);
+
+    books.appendChild(bookElement);
 }
+
+const myLibrary = [];
+let id = 0;
 
 
 
@@ -60,12 +67,16 @@ add.addEventListener('click', (e)=>{
     let author = document.getElementById('author').value;
     let pages = document.getElementById('pages').value;
     let read = document.getElementById('read').checked;
+    const existingBook = myLibrary.find(book => book.title === title && book.author === author);
 
     if (title == "" || author == "" || pages == ""){
         alert('You should not leave empty spaces.');
     }
     if (isNaN(pages)){
         alert('The number of pages should be a numeric value');
+    }
+    if (existingBook){
+        alert('This book already exists in the library')
     }
 
     else {
@@ -79,14 +90,3 @@ const newBookButton = document.getElementById('newBook');
 newBookButton.addEventListener('click', ()=>{
     dialog.showModal();
 });
-
-
-
-
-
-Book.prototype.callBook = function (){
-    console.log(this.title, this.author, this.pages, this.selection, this.id);
-}
-
-
-
